a5d46ca84a8f88eb97a8c7a40fdc675d75b518d9
macaulefirmo <macaule.firmo@gmail.com>
2018-04-09 20:24:13 -0300

    Ajustes basicos e comentarios

--- src/controllers/ControllerAnalisadorLexico.java
+++ src/controllers/ControllerAnalisadorLexico.java
@@ -2,24 +2,30 @@ package controllers;
 
 public class ControllerAnalisadorLexico {
     
-    public String lerCaracteres(String texto){
+    /**
+     * Le caractere por caractere de uma String.
+     * @param texto String a ser lida
+     * @return 
+     */
+    public void lerCaracteres(String texto){
         
         int i = 0;
         int linha = 0;
         do {
-            char atual = texto.charAt(i);           
-            
-            if(atual == '"') {                
+            char atual = texto.charAt(i);             
+            if(atual == '"') {   
+                // Inicio da cadeia de caracteres
                 i++;
                 char atual2 = texto.charAt(i);                
                 String cadeia = "";
                 boolean controleErro = true;
-                while(atual2 != '"') {
-                    
+                // Cadeia de caracteres - tudo que esta entre "aspas"
+                while(atual2 != '"') { 
+                    // Incrementa a contagem de linha
                     if(atual2 == '\n'){
                         linha++;
-                    }
-                    
+                    }                     
+                    // Verifica o \"
                     if(atual2 == '\\'){
                         char verifica = texto.charAt(i+1);
                         if(verifica == '"') {
@@ -29,7 +35,8 @@ public class ControllerAnalisadorLexico {
                     } else {
                         cadeia += Character.toString(atual2);
                         i++;   
-                    }    
+                    } 
+                    // Verifica se chegou ao final da String
                     if(i >= texto.length()) {
                         // Erro - Fecha aspas não encontrado.
                         System.out.println("Erro na linha "+linha+" - Fecha aspas não encontrado!");
@@ -39,27 +46,36 @@ public class ControllerAnalisadorLexico {
                         atual2 = texto.charAt(i);
                     } 
                 }
-                
+                // Verifica se houve erro.
                 if(controleErro) {
                     System.out.println("Cadeia: "+cadeia);
+                    // Verifica a validade da cadeia de caracteres.
                     if(cadeiaDeCaracteres(cadeia)){                        
                         System.out.println("Cadeia aceita!");
-                    }else {
+                    } else {
+                        // Erro - Caractere invalido.
                         System.out.println("Erro na linha "+linha+" - Caractere inválido!");  
                     }                      
                 }                           
             } else if(atual == '\n'){
-                    linha++;
+                // Incrementa o numero da linha. 
+                linha++;
             }
             i++;        
-        } while(i < texto.length());   
-        return null;
+        } while(i < texto.length());     
     }
     
+    /**
+     * Verifica se a cadeia recebida eh valida.
+     * @param cadeia String com a cadeia de caracteres
+     * @return 
+     */
     public boolean cadeiaDeCaracteres(String cadeia) {
-        
+        // Caracteres especiais - Os caracteres utilizados pela expressao regular.
         String especiais = "\\]|\\[|\\+|\\$|\\^|\\{|\\}|\\|\\?|\\.|\\(|\\)|\\*|\\-|\\\\|\"";
+        // Outros - Letras e numeros.
         String outros = "a-z|A-Z|0-9|";
+        // Asc - Os caracteres da tabela ascII que nao foram encluidos acima.
         String asc = " |#|!|%|´|`|@|/|~|_|<|>|=|:|;|,|'|&";
         return cadeia.matches("["+especiais+outros+asc+"]*");
     }
